d91dfec525f1e729c31637fd6a58a9f13e17e7f3
lavender <lavenderanyango9@gmail.com>
2024-01-31 18:52:27 +0300

    pycode style

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
-""" BasicCache module
 """
-from typing import Any
-from base_caching import BaseCaching
+Basic dictionary
+"""
 
-class BaseCaching:
-    """ BaseCaching class """
 
-    def __init__(self):
-        """ Initialize """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache """
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data[key]))
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache class """
+    """
+    Basic Cache class that inherits from BaseCaching
+    """
 
-    def put(self, key: Any, item: Any) -> None:
-        """ Add an item in the cache """
-        if key is not None and item is not None:
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key and item:
             self.cache_data[key] = item
 
-    def get(self, key: Any) -> Any:
-        """ Get an item from the cache """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
