db2273869102d0c2eacecbe7744c335ab84671c4
lavender <lavenderanyango9@gmail.com>
2024-01-31 20:45:55 +0300

    LIFO cacheing

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" LIFO Caching """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO Caching system that inherits from BaseCaching"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_item = self.get_last_item()
+            del self.cache_data[last_item]
+            print("DISCARD:", last_item)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
+
+    def get_last_item(self):
+        """ Get the last item inserted into cache"""
+        return next(reversed(self.cache_data))
