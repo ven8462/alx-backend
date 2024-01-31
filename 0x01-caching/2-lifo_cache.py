2c81a058deef3710142b956faafa7656f153ee0c
lavender <lavenderanyango9@gmail.com>
2024-01-31 21:39:43 +0300

    lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
-""" LIFO Caching """
+"""
+LIFO Caching
+"""
 
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFO Caching system that inherits from BaseCaching"""
+    """ LIFO Caching"""
+
     def __init__(self):
+        """
+        init method
+        """
         super().__init__()
+        self.key_indexes = []
 
     def put(self, key, item):
-        """ Add an item in the cache"""
-        if key is None or item is None:
-            return
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key
+        """
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_item = self.get_last_item()
-            del self.cache_data[last_item[0]]
-            print("DISCARD:", last_item[0])
-
-        self.cache_data[key] = item
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
-        """ Get an item by key"""
-        if key is None or key not in self.cache_data:
-            return None
-
-        return self.cache_data[key]
-
-    def get_last_item(self):
-        """ Get the last item inserted into cache"""
-        return list(self.cache_data.items())[-1]
+        """
+        Must return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
